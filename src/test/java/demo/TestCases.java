d102e9b7668d13c83afc9876ea10be1ebbe51aae
ashish1995sachan <ashishsachan201@gmail.com>
2024-08-16 00:21:12 +0530

    ME_QA_XGOOGLE_FORM_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -1,6 +1,7 @@
 package demo;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -10,24 +11,109 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
 import java.util.logging.Level;
 // import io.github.bonigarcia.wdm.WebDriverManager;
 import demo.wrappers.Wrappers;
 
 public class TestCases {
     ChromeDriver driver;
-
+    
     /*
      * TODO: Write your tests here with testng @Test annotation. 
      * Follow `testCase01` `testCase02`... format or what is provided in instructions
      */
+    @Test
+    public void testCase01() throws InterruptedException{
+        Thread.sleep(2000);
+        driver.get("https://docs.google.com/forms/d/e/1FAIpQLSep9LTMntH5YqIXa5nkiPKSs283kdwitBBhXWyZdAS-e4CxBQ/viewform");
+        Thread.sleep(2000);
+        WebElement name = driver.findElement(By.xpath("//div[@class='Xb9hP']/input[@type='text']"));
+        Wrappers.enterString(name, "Ashish Sachan");
+        Thread.sleep(2000);
+    }
 
+    @Test
+    public void testCase02() throws InterruptedException{
+        WebElement practiceAutomation = driver.findElement(By.xpath("//textarea[@jsname='YPqjbf']"));
+        String reason = "I want to be the best QA Engineer";
+        long epochtime=System.currentTimeMillis()/1000;
+        String timeString = String.valueOf(epochtime);
+        Thread.sleep(2000);
+        Wrappers.enterString(practiceAutomation, reason+" "+timeString);
+        Thread.sleep(2000);
+    }
      
+    @Test
+    public void testCase03() throws InterruptedException{
+        
+        Wrappers.clickRadio(driver, "3 - 5");
+        Thread.sleep(2000);
+    }
+
+    @Test
+    public void testCase04() throws InterruptedException{
+        
+        Wrappers.clickCheckBox(driver, "Java");
+        Thread.sleep(2000);
+        Wrappers.clickCheckBox(driver, "Selenium");
+        Thread.sleep(2000);
+        Wrappers.clickCheckBox(driver, "TestNG");
+        Thread.sleep(2000);
+    }
+    
+    @Test
+    public void testCase05() throws InterruptedException{
+        WebElement dropDownMenu = driver.findElement(By.xpath("//div[contains(@class,'t9kgXb')]"));
+        dropDownMenu.click();
+        Wrappers.dropDownOption(driver, "Mr");
+        Thread.sleep(2000);
+    }
+
+    @Test
+    public void testCase06() throws InterruptedException{
+        WebElement dateOption = driver.findElement(By.xpath("//input[@type='date']"));
+        String dateSevenDaysAgo = Wrappers.dateSevenDaysAgo();
+        Wrappers.enterString(dateOption, dateSevenDaysAgo);
+        Thread.sleep(2000);
+    }
+
+    @Test
+    public void testCase07() throws InterruptedException{
+        String time = Wrappers.currentTime();
+        String[] timeFormat = time.split((":"));
+        String HH = timeFormat[0];
+        String MM = timeFormat[1];
+        WebElement hours = driver.findElement(By.xpath("//input[contains(@role,'combobox')]"));
+        Wrappers.enterString(hours, HH);
+        WebElement min = driver.findElement(By.xpath("(//input[contains(@role,'combobox')])[2]"));
+        Wrappers.enterString(min, MM);
+        Thread.sleep(2000);
+    }
+
+    @Test
+    public void testCase8() throws InterruptedException{
+    WebElement submit = driver.findElement(By.xpath("//span[contains(text(),'Submit')]"));
+    submit.click();
+    Thread.sleep(4000);
+    }
+
+    @Test
+    public void testCase9() throws InterruptedException{
+    WebElement submitsuccess = driver.findElement(By.xpath("//a[contains(text(),'Submit another response')]"));
+    if(submitsuccess.isDisplayed()){
+        System.out.println("Thanks for your response, Automation Wizard!");;
+    }
+    else{
+        System.out.println("Submission of form Unsuccessful");
+    }
+    
+    }
     /*
      * Do not change the provided methods unless necessary, they will help in automation and assessment
      */
     @BeforeTest
-    public void startBrowser()
+    public void startBrowser() throws InterruptedException
     {
         System.setProperty("java.util.logging.config.file", "logging.properties");
 
@@ -47,13 +133,15 @@ public class TestCases {
         driver = new ChromeDriver(options);
 
         driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+        
     }
 
     @AfterTest
     public void endTest()
     {
         driver.close();
-        driver.quit();
+        //driver.quit();
 
     }
 }
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -3,9 +3,14 @@ package demo.wrappers;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+
 
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -16,4 +21,44 @@ public class Wrappers {
     /*
      * Write your selenium wrappers here
      */
+    
+    public static void enterString(WebElement name , String text){
+        name.clear();
+        name.sendKeys(text);
+
+    }
+
+    public static void clickRadio(WebDriver driver, String optionText){
+        WebElement radioButton = driver.findElement(By.xpath("//span[contains(@class,'OvPDhc') and contains(text(),'" + optionText + "')]/../../..//div[@class='vd3tt']"));
+        radioButton.click();
+    }
+
+    public static void clickCheckBox(WebDriver driver, String optionTextCheckBox){
+        WebElement checkBox = driver.findElement(By.xpath("//span[contains(@class,'n5vBHf') and contains(text(),'" + optionTextCheckBox + "')]/../../preceding-sibling::div[contains(@id,'i')]"));
+        checkBox.click();
+    }
+
+    public static void dropDownOption(WebDriver driver, String optionTextDropDown){
+        WebElement dropdown = driver.findElement(By.xpath("//div[contains(@class,'QXL7Te')]//div[@data-value='" + optionTextDropDown + "']"));
+        dropdown.click();
+    }
+
+    public static String dateSevenDaysAgo(){
+        LocalDate currentDate = LocalDate.now();
+        LocalDate sevenDaysAgo = currentDate.minusDays(7);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        String date = sevenDaysAgo.format(formatter);
+        return date;
+    }
+
+    public static String currentTime(){
+        LocalTime currentTime1 = LocalTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+        String currentTime = currentTime1.format(formatter);
+        return currentTime;
+    }
+
+
+
+
 }
